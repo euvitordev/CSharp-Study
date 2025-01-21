b7e4f3125ef4a8d7824138340baf346442c8d7c2
Vitor Valente <dev.vitorlucas@gmail.com>
2025-01-20 21:41:47 -0300

    feat: add a method Reversed String

---
+++ Algoritmos/ReversedStrings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.Algoritmos
+{
+    public class ReversedStrings
+    {
+        // Exercicio CodeWarns: Reversed Strings
+        // Link: https://www.codewars.com/kata/5168bb5dfe9a00b126000018/solutions/csharp
+
+        // Solution
+        public static string Solution(string value)
+        {
+            string solution = "";
+
+            for (int i = value.Length - 1; i >= 0; i--)
+                solution += value[i];
+
+            return solution;
+        }
+    }
+}
