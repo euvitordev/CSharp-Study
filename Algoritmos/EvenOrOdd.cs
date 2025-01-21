bc9b78817ac0c82df500cf4f030a60dc03a9cb5d
Vitor Valente <dev.vitorlucas@gmail.com>
2025-01-21 20:52:14 -0300

    feat: add a method Even or Odd

---
+++ Algoritmos/EvenOrOdd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.Algoritmos
+{
+    public class EvenOrOdd
+    {
+        public static string Solution(int number)
+        {
+            if (number % 2 == 0) return "Even";
+            else return "Odd";
+        }
+    }
+}
