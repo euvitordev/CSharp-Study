137be7d559efee635114fb093e819866d275a4dc
Vitor Valente <dev.vitorlucas@gmail.com>
2025-01-21 20:17:11 -0300

    feat: add new method ValidatePin

---
+++ Algoritmos/ValidatePin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.Algoritmos
+{
+    public class ValidatePin
+    {
+        public static bool Solution(string pin)
+        {
+            bool isValid = false;
+            if (pin.Length == 4 || pin.Length == 6)
+            {
+                for (int i = 0; i < pin.Length; i++)
+                {
+                    if (char.IsDigit(pin[i])) isValid = true;
+                    else
+                    {
+                        return false;
+                        break;
+                    }
+
+                }
+            }
+            return isValid;
+        }
+    }
+}
