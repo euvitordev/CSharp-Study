35a1165c66299ddf8324c427c79005849c2ed7d2
Vitor Valente <dev.vitorlucas@gmail.com>
2025-01-22 21:15:50 -0300

    feat: add a method Square(n) Sum

---
+++ Algoritmos/SquareSum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.Algoritmos
+{
+    // Exercicio CodeWarns: Square(n) Sum
+    // Link: https://www.codewars.com/kata/515e271a311df0350d00000f/solutions/csharp
+    public class SquareSum
+    {
+        public static int Solution(int[] numbers)
+        {
+            if (numbers.Length == 0) return 0;
+
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int square = numbers[i] * numbers[i];
+                sum += square;
+            }
+            return sum;
+        }
+    }
+}
