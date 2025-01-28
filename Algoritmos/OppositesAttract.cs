8877732daf551ca1516769d87f02f7590d057b31
Vitor Valente <dev.vitorlucas@gmail.com>
2025-01-27 22:40:55 -0300

    feat: add a method LoveDetector

---
+++ Algoritmos/OppositesAttract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study.Algoritmos
+{
+    public class OppositesAttract
+    {
+        public static bool lovefunc(int flower1, int flower2)
+        {
+            
+        }
+    }
+}
