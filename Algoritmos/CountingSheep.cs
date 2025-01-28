27ff9d56e42e498aeeb98e270019a861ebf906ca
Vitor Valente <dev.vitorlucas@gmail.com>
2025-01-27 21:34:25 -0300

    feat: add method Counting sheep...

---
+++ Algoritmos/CountingSheep.cs
@@ -0,0 +1,17 @@
+﻿namespace C_Sharp_Study.Algoritmos
+{
+    public class CountingSheep
+    {
+        public static int CountSheeps(bool[] sheeps)
+        {
+            int count = 0;
+
+            foreach (bool sheep in sheeps)
+            {
+                if (sheep == true) count++;
+            }
+
+            return count;
+        }
+    }
+}
